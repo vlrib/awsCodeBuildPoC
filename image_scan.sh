3ad4e1028e7b5a8cc9fb92a381835f1003330f93
vlrib <viniribeiro101@gmail.com>
2021-08-03 11:35:59 -0300

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
+echo "Scanning image..."
 aws ecr start-image-scan --repository-name $IMAGE_REPO_NAME  --image-id imageTag=$IMAGE_TAG
+
+echo "Waiting for completion of image scan..."
 aws ecr wait image-scan-complete --repository-name $IMAGE_REPO_NAME --image-id imageTag=$IMAGE_TAG
+echo "Image scan completed."
 
 if [ $(echo $?) -eq 0 ]; then
+  echo "Checking for high or critical vulnerabilities..."
   SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --repository-name $IMAGE_REPO_NAME --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts')
   CRITICAL=$(echo $SCAN_FINDINGS | jq '.CRITICAL')
   HIGH=$(echo $SCAN_FINDINGS | jq '.HIGH')
     if [ $CRITICAL != null ] || [ $HIGH != null ]; then
-      echo Docker image contains vulnerabilities at CRITICAL or HIGH level
+      echo "Docker image contains vulnerabilities at CRITICAL or HIGH level"
+      echo "Pipeline will be aborted"
       PIPELINE_EXECUTION_ID=$(aws codepipeline list-pipeline-executions --pipeline-name poc-ws | jq '.pipelineExecutionSummaries[0].pipelineExecutionId' | sed 's/"//g')
       #aws ecr batch-delete-image --repository-name $IMAGE_REPO_NAME --image-ids imageTag=$IMAGE_TAG
       aws codepipeline stop-pipeline-execution --pipeline-name poc-ws --pipeline-execution-id $PIPELINE_EXECUTION_ID --abandon --reason "Stopping execution for find image vulnerability"
-      exit 1
+      echo "Pipeline successfully aborted. Check vulnerability report for $IMAGE_REPO_NAME:$IMAGE_TAG image in ECR repository"
+    else
+      echo "Docker image is safe to be deploy"
     fi
 fi
