7ee817988b0666728cb2cdddbf9779d7a3e1d45d
vlrib <viniribeiro101@gmail.com>
2021-08-03 11:22:17 -0300

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -9,7 +9,9 @@ if [ $(echo $?) -eq 0 ]; then
   HIGH=$(echo $SCAN_FINDINGS | jq '.HIGH')
     if [ $CRITICAL != null ] || [ $HIGH != null ]; then
       echo Docker image contains vulnerabilities at CRITICAL or HIGH level
-      aws ecr batch-delete-image --repository-name $IMAGE_REPO_NAME --image-ids imageTag=$IMAGE_TAG
+      PIPELINE_EXECUTION_ID=$(aws codepipeline list-pipeline-executions --pipeline-name poc-ws | jq '.pipelineExecutionSummaries[0].pipelineExecutionId' | sed 's/"//g')
+      #aws ecr batch-delete-image --repository-name $IMAGE_REPO_NAME --image-ids imageTag=$IMAGE_TAG
+      aws codepipeline stop-pipeline-execution --pipeline-name poc-ws --pipeline-execution-id $PIPELINE_EXECUTION_ID --abandon --reason "Stopping execution for find image vulnerability"
       exit 1
     fi
 fi
