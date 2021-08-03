8f9db64846ab6e002d77a34ea35591bd9bb1c5eb
vlrib <viniribeiro101@gmail.com>
2021-08-03 10:10:03 -0300

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-aws ecr start-image-scan --repository-name $REPOSITORY_NAME  --image-id imageTag=$IMAGE_TAG
-aws ecr wait image-scan-complete --repository-name $REPOSITORY_NAME --image-id imageTag=$IMAGE_TAG
+aws ecr start-image-scan --repository-name $IMAGE_REPOSITORY_NAME  --image-id imageTag=$IMAGE_TAG
+aws ecr wait image-scan-complete --repository-name $IMAGE_REPOSITORY_NAME --image-id imageTag=$IMAGE_TAG
 
 if [ $(echo $?) -eq 0 ]; then
-  SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --repository-name $REPOSITORY_NAME --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts')
+  SCAN_FINDINGS=$(aws ecr describe-image-scan-findings --repository-name $IMAGE_REPOSITORY_NAME --image-id imageTag=$IMAGE_TAG | jq '.imageScanFindings.findingSeverityCounts')
   CRITICAL=$(echo $SCAN_FINDINGS | jq '.CRITICAL')
   HIGH=$(echo $SCAN_FINDINGS | jq '.HIGH')
     if [ $CRITICAL != null ] || [ $HIGH != null ]; then
       echo Docker image contains vulnerabilities at CRITICAL or HIGH level
-      aws ecr batch-delete-image --repository-name $REPOSITORY_NAME --image-ids imageTag=$IMAGE_TAG
+      aws ecr batch-delete-image --repository-name $REPOSITORY_REPO_NAME --image-ids imageTag=$IMAGE_TAG
       exit 1
     fi
 fi
